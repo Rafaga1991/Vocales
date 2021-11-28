91247a6c09a01ba6a1b0f374027f50373e943281
Rafael Minaya Beltrán <lomasduro14.rm@gmail.com>
2021-11-28 18:12:49 -0400

    completado

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+    $ratings = json_decode(file_get_contents('./assets/data/rating.json'), true);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,15 +9,76 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Práctica</title>
-
+    <title>Carlos Gil</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 
 <body>
     <div class="container">
-        <h2 class="text-uppercase mt-5 text-center text-muted fw-bold">pr&aacute;ctica de vocales</h2>
+        <hr class="dropdown-divider mt-5">
+        <h2 class="text-uppercase text-center text-muted fw-bold">informaci&oacute;n</h2>
+        <hr class="dropdown-divider">
+
+        <h1 class="fw-bold text-uppercase text-muted">Carlos</h1>
+        <hr class="dropdown-divider">
+
+        <h4 class="text-muted fw-bold">BIOGRAF&iacute;A</h4>
+        <img width="200px" src="C:\Users\0021c\Documents\UASD\Semestre 2021-2\Lenguaje de Programación III\Tarea de LAB 1\Carlos.jpg" alt="Carlos">
+
+        <p class="alert alert-success">Mi nombre es <span class="fw-bold">Carlos</span>,
+            soy estudiante de licenciatura en informática en la Universidad Autonoma de Santo Domingo (UASD-SFM),
+            me gusta la programación, y soy aficionado a la tecnologia y a las noticias de indole tecnológica.
+        </p>
+
+        <p>Si desea saber sobre la facultad, <a href="https://www.uasd.edu.do/index.php/ciencias"> <span class="badge bg-primary">Click aquí</span> </a></p>
+
+        <p>Areas de la informatica que me resultan interesantes, son las sigueintes:</p>
+        <ul class="list-group my-2">
+            <li class="list-group-item">Área de Arquitectura y Tecnología de los Computadores</li>
+            <li class="list-group-item">Área de Lenguajes y Sistemas Informáticos</li>
+        </ul>
+
+        <p>En realidad me gustan todas pero esa me crean una mayor curiosidad e interés</p>
+
+        <hr class="dropdown-divider">
+        <h4 class="text-muted fw-bold">ESTUDIOS ACADEMICOS</h4>
+        <hr class="dropdown-divider">
+
+        <p>Soy estudiante de licenciatura en informática, estoy en mi 6to semestre en materias superiores,
+            en este semestre estoy cursando 4 materias, las cuales son:
+        </p>
+
+
+        <ol class="list-group my-1">
+            <li class="list-group-item"> BASES DE DATOS I</li>
+            <li class="list-group-item"> LENGUAJE PROGRAMACIÓN III </li>
+            <li class="list-group-item"> LAB. LENGUAJE PROGRAMACIÓN III </li>
+            <li class="list-group-item"> CALCULO I </li>
+        </ol>
+
+        <p>En el semestre anterior tuve las siguientes calificaciones: </p>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>Materia</th>
+                    <th>Calificación</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($ratings as $rating):?>
+                    <tr>
+                        <td><?=$rating['course']?></td>
+                        <td><?=$rating['rating']?></td>
+                    </tr>
+                <?php endforeach;?>
+            </tbody>
+        </table>
+
+        <hr class="dropdown-divider mt-5">
+        <h2 class="text-uppercase text-center text-muted fw-bold">CONTADOR DE PALABRAS</h2>
+        <hr class="dropdown-divider">
+
         <div class="form-group">
             <label for="txtText">Oraci&oacute;n</label>
             <div class="input-group">
